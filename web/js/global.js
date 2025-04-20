81f6ffdcef652fb1b8d2489e1f871dcee482496c
zSemper <132165242+zSemper@users.noreply.github.com>
2025-04-20 22:11:10 +0200

    test

--- web/js/global.js
+++ web/js/global.js
@@ -3,6 +3,10 @@ export const VERSIONS = {
         mod: "0.0.0",
         minecraft: "0.0.0"
     },
+    common_ore_library: {
+        mod: "2.1.1",
+        minecraft: "1.21.1"
+    },
     passive_generators: {
         mod: "1.0.0",
         minecraft: "1.21.1"
